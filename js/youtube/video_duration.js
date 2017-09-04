9e7d093529122ef574e7682fd728cfd66b3ec727
raffaele messuti <raffaele@atomotic.com>
2017-09-04 11:10:41 +0200

    youtube: video duration test

--- js/youtube/video_duration.js
+++ js/youtube/video_duration.js
@@ -1,14 +1,25 @@
 video = window.frames[0].document.getElementsByTagName("video")[0];
 
-async function get_duration() {
-  var duration = new Promise(resolve => {
-    video.addEventListener("loadedmetadata", function() {
-      resolve(video.duration);
-    });
-  });
-  return await duration;
-}
+// async function get_duration() {
+//   var duration = new Promise(resolve => {
+//     video.addEventListener("loadedmetadata", function() {
+//       resolve(video.duration);
+//     });
+//   });
+//   return await duration;
+// }
 
-get_duration().then(v => {
-   return v;
-});
+// get_duration().then(v => {
+//    return v;
+// });
+
+
+var callback = arguments[arguments.length - 1];
+
+video.onloadedmetadata = function() {
+  callback(video.duration);
+};
+
+setTimeout(function() {
+  callback(0);
+}, 3000);
